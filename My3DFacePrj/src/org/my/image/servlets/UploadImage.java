3cb47977486560990879cc89d9333bac53cef5c2
avihaim <avihaim@gamil.com>
2012-09-05 01:59:18 +0300

    image filter

--- My3DFacePrj/src/org/my/image/servlets/UploadImage.java
+++ My3DFacePrj/src/org/my/image/servlets/UploadImage.java
@@ -10,7 +10,6 @@ import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.net.URL;
-import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Random;
@@ -32,6 +31,7 @@ import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jackson.node.ArrayNode;
 import org.codehaus.jackson.node.ObjectNode;
 import org.my.image.app.FaceDataManager;
+import org.my.image.app.ImageUtil;
 import org.my.image.exceptions.BadFileException;
 
 import sun.misc.BASE64Decoder;
@@ -46,7 +46,6 @@ public class UploadImage extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private static Random generator = new Random();
 	private static ObjectMapper mapper = new ObjectMapper();
-	private static List<String> imageTyps = Arrays.asList(".jpg",".png",".jpge");
     
 	
 	public void doPost(HttpServletRequest request, HttpServletResponse response)
@@ -173,10 +172,8 @@ public class UploadImage extends HttpServlet {
 		String domainName = itemName.substring(IndexOf);
 		System.out.println("domainName: " + domainName);
 		
-		if(!imageTyps.contains(domainName.toLowerCase())) {
-			throw new BadFileException("This file type is not allow, only one of the follwing : " + imageTyps);
-		}
-
+		ImageUtil.isImage(domainName);
+		
 		String finalimage = buffer.toString() + "_" + r
 				+ domainName;
 		System.out.println("Final Image===" + finalimage);
