934832f9ba05616526d35051d286a1be8331b003
avihaim <avihaim@gmail.com>
2012-09-05 13:40:39 +0300

    Update My3DFacePrj/src/org/my/image/app/MakeDepthMapWrapper.java

--- My3DFacePrj/src/org/my/image/app/MakeDepthMapWrapper.java
+++ My3DFacePrj/src/org/my/image/app/MakeDepthMapWrapper.java
@@ -4,15 +4,6 @@ public class MakeDepthMapWrapper {
 	
 	static {
 		System.loadLibrary("facedetect");
-//		String property = System.getProperty("java.library.path");
-//		
-//		if(!property.contains(":/users/stud09co/avihaimo")) {
-//			System.setProperty("java.library.path", property + ":/users/stud09co/avihaimo");
-//		}----------------
-//		
-//		System.out.println(System.getProperty("java.library.path"));
-//		System.loadLibrary("MyProj");
-//		System.loadLibrary("org.my.image.servlets.MakeDepthMapWrapper");
 	}
 	
 	public static native String makeDepthMap(String fileName,String resultPath);
